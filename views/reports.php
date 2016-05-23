b742014c5f1fcee6583e93e3c4261b52f95f44dd
hectornauta <del_emiliano@outlook.com.ar>
2016-05-23 18:23:09 -0300

    Arreglado lo de mostrar informes
    Bueno eso

--- views/reports.php
+++ views/reports.php
@@ -610,27 +610,19 @@ require_once(__ROOT__.'/config.php');
 	<br>
 			<input type="submit" class="btn btn-success submit" name="print" value="Imprimir informe" id="btnimprimir"/>
 	</div>
-<?php
-echo '
-    <script src="http://onehackoranother.com/projects/jquery/tipsy/javascripts/jquery.tipsy.js"></script>
-    <script src="'. $site_url .'/js/bootbox.min.js"></script>';
-?>
+	
+<script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/jquery-ui.min.js">
+	$(function() {
+	$("#wash_day1").datepicker();
+	});
 
-<script type="text/javascript">
+	$(function() {
+	$("#wash_day2").datepicker();
+	});
+</script>
 
-$(function() {
-    $("#wash_day1").datepicker();
-    $(".selectpicker").selectpicker({
-      size: 4
-    });
-  });
-  
-  $(function() {
-    $("#wash_day2").datepicker();
-    $(".selectpicker").selectpicker({
-      size: 4
-    });
-  });
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.3/jquery.min.js"></script>
+<script>	
 $(document).ready(function(){
 $("#btnclientes").click(function(){
 	$("#informecentros").hide();
