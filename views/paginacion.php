2addf306752ec98a08aa73342ecc2a1d0820ef11
hectornauta <del_emiliano@outlook.com.ar>
2016-05-30 17:04:14 -0300

    Tamaño de paginacion
    Porque karina lo quería chiquitito

--- views/paginacion.php
+++ views/paginacion.php
@@ -1,11 +1,11 @@
 <html>
 <div class="center">
 <ul class="pagination pagination-lg">
-  <li class="disabled"><a href="javascript:void(0)">«</a></li>
-  <li class="active"><a href="javascript:void(0)">1</a></li>
-  <li class="disabled" ><a href="javascript:void(0)">2</a></li>
-  <li class="disabled" ><a href="javascript:void(0)">3</a></li>
-  <li class="disabled" ><a href="javascript:void(0)">»</a></li>
+  <li class="disabled"><a href="javascript:void(0)"  style="font-size:12px">«</a></li>
+  <li class="active"><a href="javascript:void(0)" style="font-size:12px">1</a></li>
+  <li class="disabled" ><a href="javascript:void(0)" style="font-size:12px">2</a></li>
+  <li class="disabled" ><a href="javascript:void(0)" style="font-size:12px">3</a></li>
+  <li class="disabled" ><a href="javascript:void(0)" style="font-size:12px">»</a></li>
 </ul>
 </div>
 
