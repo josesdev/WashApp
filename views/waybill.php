3ab332e954dcb08a3e528babb3a1ff8d46f86714
hectornauta <del_emiliano@outlook.com.ar>
2016-06-22 13:34:59 -0300

    Hoja de ruta
    Hecha más responsiva

--- views/waybill.php
+++ views/waybill.php
@@ -18,7 +18,8 @@ require_once(__ROOT__.'/config.php');
 						<select class="selectpicker" name="type_station">
 							<option>Estación 1</option>
 							<option>Estación 2</option>
-							<option>Estación 3</option>
+							<option class="mostrar" style='display:none;' disabled="true">Estación 3 - asignada - </option>
+							<option class="ocultar">Estación 3</option>
 							<option>Estación 4</option>
 							<option>Estación 5</option>
 							<option>Estación 6</option>
@@ -62,13 +63,45 @@ require_once(__ROOT__.'/config.php');
 								<th>Cliente</th>
 							</thead>
 							<tbody>
-								<tr>
+								<tr class="ocultar" >
 									<td><label><input type="checkbox" value=""></label></td>
 									<td>08:00</td>
-									<td>López y Planes 800 <a href="http://www.noseencuentra.com">Ver en el mapa</a></td>
+									<td>López y Planes 800 <a href="https://goo.gl/maps/Nbz34PNN3Gt">Ver en el mapa</a></td>
+									<td>Básico</td>
+									<td>Pickup</td>
+									<td>Gonzalo González</td>
+								</tr>
+								<tr class="ocultar" >
+									<td><label><input type="checkbox" value=""></label></td>
+									<td>09:00</td>
+									<td>San Lorenzo 800 <a href="https://goo.gl/maps/Rweb85daZEw">Ver en el mapa</a></td>
 									<td>Básico</td>
 									<td>Sedán</td>
-									<td>Jorge González</td>
+									<td>Benito Benítez</td>
+								</tr>
+								<tr class="ocultar" >
+									<td><label><input type="checkbox" value=""></label></td>
+									<td>10:00</td>
+									<td>French 600<a href="https://goo.gl/maps/6zFYVDASJh32">Ver en el mapa</a></td>
+									<td>Ecológico</td>
+									<td>Sedán</td>
+									<td>Fernando Fernández</td>
+								</tr>
+								<tr class="mostrar2" style='display:none;' >
+									<td><label><input type="checkbox" value=""></label></td>
+									<td>10:00</td>
+									<td>French 600<a href="https://goo.gl/maps/6zFYVDASJh32">Ver en el mapa</a></td>
+									<td>Ecológico</td>
+									<td>Sedán</td>
+									<td>Fernando Fernández</td>
+								</tr>
+								<tr class="mostrar2">
+									<td><label><input type="checkbox" value=""></label></td>
+									<td>11:00</td>
+									<td>Don Bosco 1200<a href="https://goo.gl/maps/SjdwuNK58AU2">Ver en el mapa</a></td>
+									<td>Completo</td>
+									<td>Sedán</td>
+									<td>Ramiro Ramírez</td>
 								</tr>
 							</tbody>
 						</table>
@@ -76,7 +109,7 @@ require_once(__ROOT__.'/config.php');
 				</div>
 				<div class="form-group">
 					<div class="center">
-					<button id="btnimprimir" type="button" class="btn btn-raised btn-success" data-style="toast" data-content="Su hoja de ruta se imprimirá en unos segundos" data-toggle="snackbar" data-timeout="0">Confeccionar e imprimir hoja de ruta</button>
+					<button id="btnimprimir1" type="button" class="btn btn-raised btn-success" data-style="toast" data-content="Su hoja de ruta se imprimirá en unos segundos" data-toggle="snackbar" data-timeout="0">Confeccionar e imprimir hoja de ruta</button>
 					</div>
 				</div>
 			</form>
@@ -91,35 +124,7 @@ require_once(__ROOT__.'/config.php');
 					<div class="col-lg-6">
 						<select class="selectpicker estaciones" name="type_station">
 							<option value="1">Estación 1</option>
-							<option>Estación 2</option>
-							<option>Estación 3</option>
-							<option>Estación 4</option>
-							<option>Estación 5</option>
-							<option>Estación 6</option>
-							<option>Estación 7</option>
-							<option>Estación 8</option>
-							<option>Estación 9</option>
-							<option>Estación 10</option>
-							<option>Estación 11</option>
-							<option>Estación 12</option>
-							<option>Estación 13</option>
-							<option>Estación 14</option>
-							<option>Estación 15</option>
-							<option>Estación 16</option>
-							<option>Estación 17</option>
-							<option>Estación 18</option>
-							<option>Estación 19</option>
-							<option>Estación 20</option>
-							<option>Estación 21</option>
-							<option>Estación 22</option>
-							<option>Estación 23</option>
-							<option>Estación 24</option>
-							<option>Estación 25</option>
-							<option>Estación 26</option>
-							<option>Estación 27</option>
-							<option>Estación 28</option>
-							<option>Estación 29</option>
-							<option>Estación 30</option>
+							<option value="1">Estación 3</option>
 						</select>
 					</div>
 				</div>
@@ -137,21 +142,45 @@ require_once(__ROOT__.'/config.php');
 									<th>Cliente</th>
 								</thead>
 								<tbody>
-									<tr>
-										<td><label><input type="checkbox" value=""></label></td>
-										<td>08:00</td>
-										<td>López y Planes 800 <a href="http://www.noseencuentra.com">Ver en el mapa</a></td>
-										<td>Básico</td>
-										<td>Sedán</td>
-										<td>Jorge González</td>
-									</tr>
+									<tr class="asignado" >
+									<td><label><input type="checkbox" value="" checked></label></td>
+									<td>08:00</td>
+									<td>López y Planes 800 <a href="https://goo.gl/maps/Nbz34PNN3Gt">Ver en el mapa</a></td>
+									<td>Básico</td>
+									<td>Pickup</td>
+									<td>Gonzalo González</td>
+								</tr>
+								<tr class="asignado" >
+									<td><label><input type="checkbox" value="" checked></label></td>
+									<td>09:00</td>
+									<td>San Lorenzo 800 <a href="https://goo.gl/maps/Rweb85daZEw">Ver en el mapa</a></td>
+									<td>Básico</td>
+									<td>Sedán</td>
+									<td>Benito Benítez</td>
+								</tr>
+								<tr class="asignado" >
+									<td><label><input type="checkbox" value="" checked></label></td>
+									<td>10:00</td>
+									<td>French 600<a href="https://goo.gl/maps/6zFYVDASJh32">Ver en el mapa</a></td>
+									<td>Ecológico</td>
+									<td>Sedán</td>
+									<td>Fernando Fernández</td>
+								</tr>
+								<tr class="noasignado">
+									<td><label><input type="checkbox" value=""></label></td>
+									<td>11:00</td>
+									<td>Don Bosco 1200<a href="https://goo.gl/maps/SjdwuNK58AU2">Ver en el mapa</a></td>
+									<td>Completo</td>
+									<td>Sedán</td>
+									<td>Ramiro Ramírez</td>
+								</tr>
 								</tbody>
 							</table>
 						</div>
 					</div>
 					<div class="form-group">
 						<div class="center">
-						<button id="btnimprimir" type="button" class="btn btn-raised btn-success" data-style="toast" data-content="Su hoja de ruta se imprimirá en unos segundos" data-toggle="snackbar" data-timeout="0">Confeccionar e imprimir hoja de ruta</button>
+						<button id="btnimprimir2" type="button" class="btn btn-raised btn-success" data-style="toast" data-content="Su hoja de ruta se imprimirá en unos segundos" data-toggle="snackbar" data-timeout="0">Confeccionar e imprimir hoja de ruta</button>
 						</div>
 					</div>
 				</div>
@@ -164,9 +193,18 @@ require_once(__ROOT__.'/config.php');
 $(document).ready(function(){
 	$('.estaciones').on('changed.bs.select', function (e) {
 		var a = $('.estaciones > .dropdown-menu > .dropdown-menu > li.selected').attr("data-original-index");
-		if (a==2){
+		if (a==1){
 			$("#listadeturnos").show();
 		}
+	});
+	$("#btnimprimir1").click(function(){
+	$(".ocultar").hide();
+	$(".mostrar").show();
+	});
+	$("#btnimprimir2").click(function(){
+		$("#listadeturnos").hide();
+		$(".ocultar2").hide();
+		$(".mostrar2").show();
 	});
 });
 </script>
